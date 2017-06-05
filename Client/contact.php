db9931b68aba6cfb662fe58c2c5a3809c2d64024
Zionn13 <zionnn17@gmail.com>
2017-06-05 13:28:10 +0300

    Modified email to contact@ride2work

--- Client/contact.php
+++ Client/contact.php
@@ -4,7 +4,7 @@
 	$message = $_POST[ 'message' ];
 	$message = " Email: " . $email . " || Message: " . $message;
 
-	if ( $send = mail('an.pop95@gmail.com', $message, "From:" . $email ) ) {
+	if ( $send = mail('contact@ride2work.ro', $message, "From:" . $email ) ) {
 		echo "Thank you <strong>$email</strong> for contacting with us!";
 	} else {
 		echo "Error in sending message! Please try again";
