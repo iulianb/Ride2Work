222ee1e8fb00e2c10a361fcc2d667f67e31f8f01
Zionn13 <zionnn17@gmail.com>
2017-06-14 14:14:12 +0300

    modified the spa routes.

--- Client/js/paging.js
+++ Client/js/paging.js
@@ -1,34 +1,34 @@
 // page.base("/routes");
 var contentLoad = $(".content-load");
-
+// sessionStorage.setItem("currentPage", "");
 function loadPage(href) {
 	contentLoad.load(href);
 }
 
-page("/home", function () {
-	sessionStorage.setItem("currentPage", "home");
+page("/", function () {
+	sessionStorage.setItem("currentPage", "");
 	loadPage("home.html");
 });
 
 page("/events", function () {
-	sessionStorage.setItem("currentPage", "events");
+	sessionStorage.setItem("currentPage", "/events");
 	loadPage("events.html");
 });
 
 page("/articles", function () {
-	sessionStorage.setItem("currentPage", "articles");
+	sessionStorage.setItem("currentPage", "/articles");
 	loadPage("articles.html");
 });
 
 
-var currentPage = "home";
+var currentPage = "";
 
 if (sessionStorage.getItem("currentPage")) {
 	currentPage = sessionStorage.getItem("currentPage");
 }
+console.log(sessionStorage);
 
-
-page.redirect('/' + currentPage);
+page.redirect('' + currentPage);
 
 page.start({
 	hashbang: true
